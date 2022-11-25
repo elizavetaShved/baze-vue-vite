8b091973097bf080f6a12d57f524692e89022878
eli3avetta <eli3avetta.shvedchikova@gmail.com>
2022-11-25 13:47:30 +0300

    first commit

---
+++ src/views/main-page/MainPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <main class="main-page">main-page</main>
+</template>
+
+<script>
+export default {
+  name: "MainPage",
+};
+</script>
+
+<style scoped lang="scss">
+.main-page {
+  overflow: hidden;
+}
+</style>
